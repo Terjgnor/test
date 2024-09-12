5bf88961e462613df7d5936917650d122de58f8f
Хантемиров Тимур Габидуллович <pl1273@PL1.EDU>
2024-09-12 10:04:56 +0500

    Создали php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    //
+    echo "one<br>";
+    echo 2**4 ."<br>";
+    echo 6 ." пятёрок<br>";
+
+    $f = 0.99;
+    $lol = "1";
+    echo $f + $lol ."<br>";
+
+    const A = 21;
+    echo A ."<br>";
+
+    $q = "12344";
+    echo $q[3];
+?>
